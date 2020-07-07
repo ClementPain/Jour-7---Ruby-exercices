bd9a0cf74be4d4f12049d8ba8e9021d5d3c08147
ClementPain <clementpain@outlook.com>
2020-07-07 17:10:30 +0200

    7

--- exo_17.rb
+++ exo_17.rb
@@ -6,7 +6,7 @@ annee=birthyear
 age=0
 temps=2020-annee
 
-while annee<=2020
+while annee<=	2020
 	
 		if age==temps 
 			puts "Il y a #{temps} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
